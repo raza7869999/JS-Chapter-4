0893fbeee9baa86bcf3566327ff6a088840482f5
Muhammad Hasnain Raza <157500053+raza7869999@users.noreply.github.com>
2024-03-31 13:51:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+Q:1
+var variable1 = "Hello, World!";
+var variable2 = "Testing...";
+var variable3 = 42;
+console.log (variable1)
+console.log (variable2)
+console.log (variable3)
+
+//Q:2
+// legal Variable
+// 1-letters 
+// 2-(both uppercase and lowercase)
+//  3--numbers, 
+//  4--underscores,
+// 5--Dollar signs
+
+// illegal Variable
+// 1. A variable name cannot start with a numeral.
+// 2. You cannot have a mathematical or logical operator in a variable name.
+// 3. You must not use any punctuation marks of any kind in a JavaScript variable name, other than the underscore
+// 4. JavaScript names must not contain spaces. Ever.
+// 6. JavaScript variable names are case-sensitive.
+
+
